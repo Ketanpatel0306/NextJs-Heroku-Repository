76e91b5fb10a63e4dfaf3e947ef75c9993aa133d
Ketanpatel0306 <Ketan_patel@codenticsoftware.com>
2022-07-22 11:35:20 +0530

    change sereverjs

--- server.js
+++ server.js
@@ -1,79 +1,36 @@
 const express = require("express");
 const next = require("next");
-const path = require("path");
-const url = require("url");
-const cluster = require("cluster");
-const numCPUs = require("os").cpus().length;
-
+const port = 4000;
 const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 4000;
-
-// Multi-process to utilize all CPU cores.
-if (!dev && cluster.isMaster) {
-  console.log(`Node cluster master ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.error(
-      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
-    );
+const app = next({ dev });
+const handle = app.getRequestHandler();
+app.prepare().then(() => {
+  const server = express();
+  server.get("/", (req, res) => {
+    app.render(req, res, "/");
   });
-} else {
-  const nextApp = next({ dir: ".", dev });
-  const nextHandler = nextApp.getRequestHandler();
-
-  nextApp.prepare().then(() => {
-    const server = express();
-
-    if (!dev) {
-      // Enforce SSL & HSTS in production
-      server.use(function (req, res, next) {
-        var proto = req.headers["x-forwarded-proto"];
-        if (proto === "https") {
-          res.set({
-            "Strict-Transport-Security": "max-age=31557600", // one-year
-          });
-          return next();
-        }
-        res.redirect("https://" + req.headers.host + req.url);
-      });
-    }
-
-    // Static files
-    // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
-    server.use(
-      "/static",
-      express.static(path.join(__dirname, "static"), {
-        maxAge: dev ? "0" : "365d",
-      })
-    );
-
-    // Example server-side routing
-    server.get("/a", (req, res) => {
-      return nextApp.render(req, res, "/b", req.query);
-    });
-
-    // Example server-side routing
-    server.get("/b", (req, res) => {
-      return nextApp.render(req, res, "/a", req.query);
-    });
-
-    // Default catch-all renders Next app
-    server.get("*", (req, res) => {
-      // res.set({
-      //   'Cache-Control': 'public, max-age=3600'
-      // });
-      const parsedUrl = url.parse(req.url, true);
-      nextHandler(req, res, parsedUrl);
-    });
-
-    server.listen(port, (err) => {
-      if (err) throw err;
-      console.log(`Listening on http://localhost:${port}`);
-    });
+  // server.get("/blog/:id", (req, res) => {
+  //   const id = req.params.id;
+  //   // console.log("serverID", id);
+  //   const queryParams = {
+  //     itemId: id,
+  //   };
+  //   app.render(req, res, "/Blog", queryParams);
+  // });
+  // server.get("/aboutUs", (req, res) => {
+  //   app.render(req, res, "/AboutUs");
+  // });
+  // server.get("/careers", (req, res) => {
+  //   app.render(req, res, "/Careers");
+  // });
+  // server.get("/archives", (req, res) => {
+  //   app.render(req, res, "/Archives");
+  // });
+  server.all("*", (req, res) => {
+    return handle(req, res, "/");
   });
-}
+  server.listen(4000, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${4000}`);
+  });
+});

3c3c0ab3861e132e61a57a795a462cdab2a9b084
Ketanpatel0306 <Ketan_patel@codenticsoftware.com>
2022-07-22 11:21:21 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { FetchApi } from "../component";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <FetchApi />
+    </div>
+  );
+}
